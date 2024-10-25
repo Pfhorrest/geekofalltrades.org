cd0bc3577cae390a6d58523082dea1c8ea0e9ef3
forrest <forrest.cameranesi@twisted-rope.com>
2024-10-25 00:21:20 -0700

    Update error.php

--- ___structure/modules/error.php
+++ ___structure/modules/error.php
@@ -1,11 +1,14 @@
 <?php
+    $errorSegments = $segments;
+    array_pop($errorSegments);
+    $parentPath = implode("/", $errorSegments);
     echo '<section>
         <h2>File Not Found</h2>
         <p class="description">No such file as '.$path.'</p>
         <p>
             You can try going <a href="javascript:history.back()">back</a>,
-            r see if you can find what you\'re looking for in the
-            <a href="./">directory listing</a>.
+            or see if you can find what you\'re looking for in the
+            <a href="/'.$parentPath.'">parent directory</a>.
         </p>
     </section>';
 ?>
