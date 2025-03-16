a3e50db07b8384513688ec1251fbaa968cb82c42
forrest <forrest.cameranesi@twisted-rope.com>
2025-03-16 02:21:56 -0700

    update photos 2012 base

--- photos/2012/__main.php
+++ photos/2012/__main.php
@@ -4,66 +4,121 @@
 	<?php
 		$images = array(
 			array(
-				'title' => 'Koi at the Japanese Friendship Garden',
-				'filename' => '11/IMG_0286.JPG',
-				'description' => 'iPhone 4s, 2012-11-24.'
-			),
-
-			array(
-				'title' => 'Sunset at the Santa Monica Pier',
-				'filename' => '10/IMG_0152.JPG',
-				'description' => 'iPhone 4s, 2012-10-06.'
-			),
-
-			array(
-				'title' => 'Flamingo and chick at the Santa Barbara Zoo',
-				'filename' => '09/IMG_0001.jpg',
-				'description' => 'iPhone 4s, 2012-09-09.'
-			),
-
-			array(
 				'title' => 'Neptune Pool at Hearst\'s Castle',
-				'filename' => '07/IMG_9102.JPG',
+				'filename' => '07/29/IMG_9102.jpeg',
 				'description' => 'Canon PowerShot A530, 2012-07-29.'
 			),
 			array(
 				'title' => 'Statue at Hearst\'s Castle',
-				'filename' => '07/IMG_9100.JPG',
+				'filename' => '07/29/IMG_9100.jpeg',
 				'description' => 'Canon PowerShot A530, 2012-07-29.'
 			),
 			array(
+				'title' => 'Foliage near Hume Lake',
+				'filename' => '07/29/IMG_9061.jpeg',
+				'description' => 'Canon PowerShot A530, 2012-07-29.'
+			),
+			array(
+				'title' => 'Hume Lake',
+				'filename' => '07/29/IMG_9058.jpeg',
+				'description' => 'Canon PowerShot A530, 2012-07-29.'
+			),
+			array(
+				'title' => 'Tiny stump in Hume Lake',
+				'filename' => '07/29/IMG_9046.jpeg',
+				'description' => 'Canon PowerShot A530, 2012-07-29.'
+			),
+
+			array(
+				'title' => 'Descending Tokopah Falls Trail',
+				'filename' => '07/28/IMG_9031.jpeg',
+				'description' => 'Canon PowerShot A530, 2012-07-28.'
+			),
+			array(
 				'title' => 'Marble Fork Kaweah River near Tokopah Falls',
-				'filename' => '07/IMG_9020.JPG',
+				'filename' => '07/28/IMG_9020.jpeg',
 				'description' => 'Canon PowerShot A530, 2012-07-28.'
 			),
 			array(
 				'title' => 'The Watchtower from Tokopah Falls Trail',
-				'filename' => '07/IMG_9006.JPG',
+				'filename' => '07/28/IMG_9006.jpeg',
 				'description' => 'Canon PowerShot A530, 2012-07-28.'
 			),
+
 			array(
-				'title' => 'Westward view from Moro Rock',
-				'filename' => '07/IMG_8985.JPG',
+				'title' => 'Northward view from Moro Rock',
+				'filename' => '07/27/IMG_8993.jpeg',
 				'description' => 'Canon PowerShot A530, 2012-07-27.'
 			),
 			array(
-				'title' => 'Moon over sequoias from Crescent Meadow',
-				'filename' => '07/IMG_8914.JPG',
+				'title' => 'Eastward view from Moro Rock',
+				'filename' => '07/27/IMG_8987.jpeg',
+				'description' => 'Canon PowerShot A530, 2012-07-27.'
+			),
+			array(
+				'title' => 'Westward view from Moro Rock',
+				'filename' => '07/27/IMG_8985.jpeg',
+				'description' => 'Canon PowerShot A530, 2012-07-27.'
+			),
+			array(
+				'title' => 'Alta Peak from Moro Rock',
+				'filename' => '07/27/IMG_8983.jpeg',
+				'description' => 'Canon PowerShot A530, 2012-07-27.'
+			),
+			array(
+				'title' => 'Foliage outside Crystal Cave',
+				'filename' => '07/27/IMG_8961.jpeg',
+				'description' => 'Canon PowerShot A530, 2012-07-27.'
+			),
+			array(
+				'title' => 'Jabba the Hutt in Crystal Cave',
+				'filename' => '07/27/IMG_8952.jpeg',
+				'description' => 'Canon PowerShot A530, 2012-07-27.'
+			),
+
+			array(
+				'title' => 'Moonrise over Crescent Meadow',
+				'filename' => '07/26/IMG_8914.jpeg',
 				'description' => 'Canon PowerShot A530, 2012-07-26.'
 			),
 			array(
-				'title' => 'Burnt out sequoia near Crescent Meadow',
-				'filename' => '07/IMG_8901.JPG',
+				'title' => 'Up a burnt sequoia in Crescent Meadow',
+				'filename' => '07/26/IMG_8901.jpeg',
 				'description' => 'Canon PowerShot A530, 2012-07-26.'
 			),
 			array(
-				'title' => 'A sprig of fern near Crescent Meadow',
-				'filename' => '07/IMG_8895.JPG',
+				'title' => 'Log in Crescent Meadow',
+				'filename' => '07/26/IMG_8900.jpeg',
 				'description' => 'Canon PowerShot A530, 2012-07-26.'
 			),
 			array(
-				'title' => 'Sequoias at Dorst Creek Campground',
-				'filename' => '07/IMG_8862.JPG',
+				'title' => 'Crescent Meadow',
+				'filename' => '07/26/IMG_8897.jpeg',
+				'description' => 'Canon PowerShot A530, 2012-07-26.'
+			),
+			array(
+				'title' => 'Spring of fern in Crescent Meadow',
+				'filename' => '07/26/IMG_8895.jpeg',
+				'description' => 'Canon PowerShot A530, 2012-07-26.'
+			),
+			array(
+				'title' => 'Sequoia trunk near the General Sherman Tree',
+				'filename' => '07/26/IMG_8871.jpeg',
+				'description' => 'Canon PowerShot A530, 2012-07-26.'
+			),
+			array(
+				'title' => 'Beyond a rooftop in Dorst Creek Campground',
+				'filename' => '07/26/IMG_8867.jpeg',
+				'description' => 'Canon PowerShot A530, 2012-07-26.'
+			),
+			array(
+				'title' => 'Up a sequoia in Dorst Creek Campground',
+				'filename' => '07/26/IMG_8862.jpeg',
+				'description' => 'Canon PowerShot A530, 2012-07-26.'
+			),
+			array(
+				'title' => 'Skyward view from Dorst Creek Campground',
+				'filename' => '07/26/IMG_8861.jpeg',
 				'description' => 'Canon PowerShot A530, 2012-07-26.'
 			),
 		);
