67b3d78843950361887bc1c846469d7c0394eb03
forrest <forrest@geekofalltrades.org>
2025-12-24 02:56:50 -0800

    testing simpler root determination

--- ___structure/html.php
+++ ___structure/html.php
@@ -22,11 +22,19 @@
 	*/
 
 	/* Determine root based on SAPI */
-	$root = root_determination(
-		php_sapi_name(),
-		$_SERVER['DOCUMENT_ROOT'] ?? '',
-		__DIR__
-	);
+	// $root = (php_sapi_name() === 'cli-server')
+	// // Running under PHP built-in server
+	// 	? realpath(__DIR__ . '/../') // Project root
+	// // Running under Apache / production
+	// : $_SERVER['DOCUMENT_ROOT'];
+
+	// $root = root_determination(
+	// 	php_sapi_name(),
+	// 	$_SERVER['DOCUMENT_ROOT'] ?? '',
+	// 	__DIR__
+	// );
+
+	$root = realpath(__DIR__ . '/../');
 
 	/**  
 	 * External path of the requested document relative to the host.
