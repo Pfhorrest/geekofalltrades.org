4167d9ffb8e75b79b20dfa4abba47dfab370c067
forrest <forrest@geekofalltrades.org>
2025-12-24 03:33:53 -0800

    Update _bootstrap.php

--- ___structure/modules/_bootstrap.php
+++ ___structure/modules/_bootstrap.php
@@ -6,7 +6,7 @@
  */
 
 /* Controller modules */
-require_once __DIR__ . '/controllers/PathFromUrl.php';
+require_once __DIR__ . '/controllers/PathFromURL.php';
 require_once __DIR__ . '/controllers/SegmentsFromPath.php';
 require_once __DIR__ . '/controllers/LightboxShouldDisplay.php';
 require_once __DIR__ . '/controllers/TitleResolution.php';
