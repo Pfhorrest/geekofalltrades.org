b393f4b8a1943dbca708015af5635ba9d0ff326f
forrest <forrest@geekofalltrades.org>
2025-12-29 05:06:54 -0800

    emergency bug tracking

--- ___structure/html.php
+++ ___structure/html.php
@@ -1,4 +1,17 @@
 <?php
+
+echo "<pre>";
+echo "REQUEST_URI = " . ($_SERVER['REQUEST_URI'] ?? 'UNSET') . "\n";
+echo "ROOTPATH    = " . ($GLOBALS['rootpath'] ?? 'UNSET') . "\n";
+echo "CWD         = " . getcwd() . "\n";
+
+$expected = rtrim($GLOBALS['rootpath'] ?? '', '/') . '/__main.php';
+echo "__main.php exists? ";
+var_dump(is_file($expected));
+echo "</pre>";
+exit;
+
+
 	/**  
 	 * The core of the PrevHP system, that loops through every subdirectory to
 	 * the requested URL and loads any styles, scripts, and meta-data found in
