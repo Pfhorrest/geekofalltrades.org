8ce91e07208e0c605630c8d008910b0f9bd8771a
forrest <forrest@geekofalltrades.org>
2026-01-09 04:46:13 -0800

    Add tests for hydrateSections
    Also other half of splitting out hydrateSectionHeadings.

--- __scripts/modules/sections/sections.js
+++ __scripts/modules/sections/sections.js
@@ -1,49 +1,9 @@
-import { toggleSection } from "./someSection/someSection";
+import { enableFlexBalancing } from "../flexBalance/flexBalance";
+import { hydrateSectionHeadings } from "./headings";
 import { hydrateToggleButtons } from "./allSections/toggleButtons/hydrateToggleButtons";
 import { expandAnchorSectionCollapseOthers } from "./allSections/allSections";
-import { toggleToggleButtons } from "./allSections/toggleButtons/toggleToggleButtons";
 import { alternateFigures } from "./figures";
-import { enableFlexBalancing } from "../flexBalance/flexBalance";
-/**
- * Initializse toggling sections by clicking their headings
- *
- * @returns {void}
- */
-const hydrateSectionHeadings = () => {
-    // console.groupCollapsed("hydrateSectionHeadings");
-    // Find all headings of sections, and for each one ...
-    document.querySelectorAll("section > h2, section > h3, section > h4, section > h5, section > h6").forEach((heading) => {
-        var _a;
-        // Get the section containing the heading
-        const section = heading.closest("section");
-        // console.log("section:", section);
-        // If that exists and has children that aren't headings or descriptions ...
-        if (section &&
-            Array.from((_a = section.children) !== null && _a !== void 0 ? _a : []).filter((child) => !(child.tagName.toLowerCase() === "h2" ||
-                child.tagName.toLowerCase() === "h3" ||
-                child.tagName.toLowerCase() === "h4" ||
-                child.tagName.toLowerCase() === "h5" ||
-                child.tagName.toLowerCase() === "h6" ||
-                (child.tagName.toLowerCase() === "p" &&
-                    child.classList.contains("description")))).length > 0) {
-            // console.log(`Found section '${heading.innerText}' to hydrate`);
-            // Give is a button role
-            heading.role = "button";
-            // Make it look clickable
-            heading.style.cursor = "pointer";
-            // Give it appropriate title and class
-            heading.title = "Collapse section";
-            heading.ariaExpanded = "true";
-            section.classList.add("toggleable");
-            // Add an event listener to toggle the section when clicked
-            heading.addEventListener("click", () => {
-                // console.log(`Toggling section '${heading.innerText}'`);
-                toggleSection(heading);
-            });
-        }
-    });
-    // console.groupEnd();
-};
+import { toggleToggleButtons } from "./allSections/toggleButtons/toggleToggleButtons";
 export const hydrateSections = () => {
     document.addEventListener("DOMContentLoaded", () => {
         enableFlexBalancing(".gallery");
--- __scripts/modules/sections/sections.ts
+++ __scripts/modules/sections/sections.ts
@@ -1,60 +1,10 @@
-import { toggleSection } from "./someSection/someSection";
+import { enableFlexBalancing } from "../flexBalance/flexBalance";
+import { hydrateSectionHeadings } from "./headings";
 import { hydrateToggleButtons } from "./allSections/toggleButtons/hydrateToggleButtons";
 import { expandAnchorSectionCollapseOthers } from "./allSections/allSections";
-import { toggleToggleButtons } from "./allSections/toggleButtons/toggleToggleButtons";
 import { alternateFigures } from "./figures";
-import { enableFlexBalancing } from "../flexBalance/flexBalance";
+import { toggleToggleButtons } from "./allSections/toggleButtons/toggleToggleButtons";
 
-/**
- * Initializse toggling sections by clicking their headings
- * 
- * @returns {void}
- */
-const hydrateSectionHeadings = (): void => {
-  // console.groupCollapsed("hydrateSectionHeadings");
-  // Find all headings of sections, and for each one ...
-  (
-    document.querySelectorAll<HTMLElement>(
-      "section > h2, section > h3, section > h4, section > h5, section > h6"
-    ) as NodeListOf<HTMLElement>
-  ).forEach((heading) => {
-    // Get the section containing the heading
-    const section = heading.closest("section");
-    // console.log("section:", section);
-    // If that exists and has children that aren't headings or descriptions ...
-    if (
-      section &&
-      Array.from(section.children ?? []).filter(
-        (child) =>
-          !(
-            child.tagName.toLowerCase() === "h2" ||
-            child.tagName.toLowerCase() === "h3" ||
-            child.tagName.toLowerCase() === "h4" ||
-            child.tagName.toLowerCase() === "h5" ||
-            child.tagName.toLowerCase() === "h6" ||
-            (child.tagName.toLowerCase() === "p" &&
-              child.classList.contains("description"))
-          )
-      ).length > 0
-    ) {
-      // console.log(`Found section '${heading.innerText}' to hydrate`);
-      // Give is a button role
-      heading.role = "button";
-      // Make it look clickable
-      heading.style.cursor = "pointer";
-      // Give it appropriate title and class
-      heading.title = "Collapse section";
-      heading.ariaExpanded = "true";
-      section.classList.add("toggleable");
-      // Add an event listener to toggle the section when clicked
-      heading.addEventListener("click", () => {
-        // console.log(`Toggling section '${heading.innerText}'`);
-        toggleSection(heading);
-      });
-    }
-  });
-  // console.groupEnd();
-};
 
 export const hydrateSections = () => {
   document.addEventListener("DOMContentLoaded", () => {
